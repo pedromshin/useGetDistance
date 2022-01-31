095a0d08687a5c2bf922123ec4cbbb625ee73dbe
Pedro Shin <pedro.shin@afya.com.br>
2022-01-31 12:20:40 -0300

    creates function

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,29 @@
-import { IUseTranslate } from "./types";
+const { useRef, useLayoutEffect } = require("react/cjs/react.production.min");
 
-function useGetDistance(
-  effect: ({ currentTranslate }: IUseTranslate) => void
-) {}
+function useGetDistance(effect, elementId) {
+  const element = document.getElementById(elementId);
+
+  const getNewTranslatePosition = () => {
+    if (element === null)
+      return {
+        elementToTop: 0,
+      };
+    return {
+      elementToTop: element.getBoundingClientRect().top,
+    };
+  };
+
+  const currentTranslate = useRef(getNewTranslatePosition());
+  const updateTranslate = () => {
+    const newTranslatePosition = getNewTranslatePosition();
+    effect({ currentTranslate: newTranslatePosition });
+    currentTranslate.current = newTranslatePosition;
+  };
+  useLayoutEffect(() => {
+    window.addEventListener("scroll", updateTranslate);
+
+    return () => window.removeEventListener("scroll", updateTranslate);
+  });
+}
 
 module.exports = useGetDistance;
