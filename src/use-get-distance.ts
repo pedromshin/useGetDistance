c89ea8209a3cbb906297357fbd0b97e18fd6874b
Pedro Shin <pedro.shin@afya.com.br>
2022-01-31 17:54:55 -0300

    type fix

---
+++ src/use-get-distance.ts
@@ -0,0 +1,27 @@
+const { useRef, useLayoutEffect } = require("react/cjs/react.production.min");
+
+export function useGetDistance(effect: ({}) => void, elementId: string) {
+  const element = document.getElementById(elementId);
+
+  const getNewTranslatePosition = () => {
+    if (element === null)
+      return {
+        elementToTop: 0,
+      };
+    return {
+      elementToTop: element.getBoundingClientRect().top,
+    };
+  };
+
+  const currentTranslate = useRef(getNewTranslatePosition());
+  const updateTranslate = () => {
+    const newTranslatePosition = getNewTranslatePosition();
+    effect({ currentTranslate: newTranslatePosition });
+    currentTranslate.current = newTranslatePosition;
+  };
+  useLayoutEffect(() => {
+    window.addEventListener("scroll", updateTranslate);
+
+    return () => window.removeEventListener("scroll", updateTranslate);
+  });
+}
